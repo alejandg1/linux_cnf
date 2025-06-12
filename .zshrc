2c80633f791e592b2278ae7829bb769f3f600800
Alejandro <josealejandrogomezparedes@gmail.com>
2025-06-12 16:16:09 -0500

    rofi menu

--- .zshrc
+++ .zshrc
@@ -1,13 +1,16 @@
 #lenguajes
-
 export PATH="/home/adre/languages/node-v23.0.0-linux-x64/bin/:$PATH"
 
-# dockers
-
 #archivos importados
-
 source ~/zsh/alias.sh
 source ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
 source ./zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/zsh/plugins.sh
 source /home/adre/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# bun completions
+[ -s "/home/adre/.bun/_bun" ] && source "/home/adre/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,8 +1,9 @@
 alias work="cd ~/workspace/"
+alias mc="cd ~/workspace/projects/Music_client/ && nvim ."
 alias obsi="cd ~/workspace/obsidian/notes/"
 alias kittyF="kitty +list-fonts --psname | grep " # solo escribe la fuente q quieres buscar
 alias backupobs="git add * && git commit -m 'backup' && git push origin main"
-alias adre="cd ~/" alias conf="nvim ~/.zshrc"
+alias conf="nvim ~/.zshrc"
 
 alias vc="cd ~/.config/nvim/  &&  nvim ."
 alias kc="cd ~/.config/kitty/  && nvim ."
@@ -16,14 +17,17 @@ alias kde-disconect="kdeconnect-cli --name adre --unpair"
 alias daily_print="bash ~/scripts/print_daily.sh"
 alias current_ker="mhwd-kernel -li"
 
-alias parrot="sudo docker start -i parrot"
-
-alias server="sudo systemctl start sshd && sudo systemctl start docker.service && sudo docker start navidrome"
+alias server="sudo systemctl start sshd && sudo systemctl start docker.service"
 
 alias passS="keepassxc-cli search ~/Documentos/pass/passwords.kdbx"
 alias passC="keepassxc-cli clip ~/Documentos/pass/passwords.kdbx"
 
 
 alias backup="~/scripts/backup.sh"
+alias reload_server="~/server/reload.sh"
 
 alias docker="sudo docker"
+
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
